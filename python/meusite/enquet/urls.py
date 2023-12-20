4a286f36ec7245b2d5283e0a97feb1d17e32ba6d
lauraTchaly <99758216+lauraTchaly@users.noreply.github.com>
2023-12-20 20:58:32 -0300

    Add files via upload

--- python/meusite/enquet/urls.py
+++ python/meusite/enquet/urls.py
@@ -3,5 +3,8 @@ from . import views
 # Create your views here.
 urlpatterns = [
     path("",views.index, name="index"),
+    path("<int:id>/", views.detalhes, name="detalhes"),
+    path("<int:id>/resultados/", views.resultados, name="resultados"),
+    path("<int:id>/voto/", views.voto, name="voto"),
 
     ]
--- python/meusite/enquet/views.py
+++ python/meusite/enquet/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import HttpResponse
 
 # Create your views here.
-def index(request):
-    return HttpResponse("Olá! vôce na página da enquete")
+# def index(request):
+#     return HttpResponse("Olá! vôce na página da enquete")
+
+
+def detalhes(request, id):
+    return HttpResponse("Você está visualizando a questão %s." % id)
+def resultados(request, id):
+    resposta = "Você está visualizando o resultado da questão %s." 
+    return HttpResponse(resposta % id)
+def voto(request, id):
+    return HttpResponse("Você está votando na questão %s." % id)
